cf4a94d7002968669d97bc0f7be9ba4ba16914cd
Sehrish Naveed <sehrish.naveed@arbisoft.com>
2020-08-28 18:19:51 +0500

    remove unused code.

--- app/server.js
+++ app/server.js
@@ -6,14 +6,6 @@ const bodyParser = require("body-parser");
 var moment = require('moment-timezone');
 var uniqid = require('uniqid');
 
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-
-    next();
-};
-
 // Enable dotenv
 dotenv.config();
 
@@ -33,7 +25,6 @@ app.set("view engine", "ejs");
 app.set("views", process.cwd() + "/app/templates");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + "/"));
-app.use(allowCrossDomain);
 
 // Add the Cronofy client setup here
 var cronofyClient = new Cronofy({
